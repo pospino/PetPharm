dc17cff02f1e08ea761473fbeb09e1eb7d18b380
prixmasol <pospino@prixmasol.com>
2015-06-16 15:28:09 -0500

    allows file:// URLs

--- js/config.js
+++ js/config.js
@@ -1,5 +1,8 @@
-angular.module('starter').constant('config',  {
-    apiurl : 'http://webapi.petpharm.net/index.php/',
-    
-});
+angular.module('starter')
+        .constant('config', {
+            apiurl: 'http://webapi.petpharm.net/index.php/',
+        })
+        .config(function ($compileProvider) {
+            $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
+        });
 
