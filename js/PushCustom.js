4ee0160421c953422c83e07bffb5b7a044c6f65a
prixmasol <pospino@prixmasol.com>
2015-06-18 23:57:11 -0500

    nada

--- js/PushCustom.js
+++ js/PushCustom.js
@@ -1,48 +1,49 @@
 // JavaScript Document
 var pushNotification;
- 
+
 function onDeviceReady() {
-        document.addEventListener("backbutton", function(e) {}, false);
-        try {
-            pushNotification = window.plugins.pushNotification;
-            if (device.platform === 'android' || device.platform === 'Android' || device.platform ===
+    document.addEventListener("backbutton", function (e) {
+    }, false);
+    try {
+        pushNotification = window.plugins.pushNotification;
+        if (device.platform === 'android' || device.platform === 'Android' || device.platform ===
                 'amazon-fireos') {
-                pushNotification.register(successHandler, errorHandler, {
-                    "senderID": "1003553143793",
-                    "ecb": "onNotification"
-                }); // required!
-            } else {
-                pushNotification.register(tokenHandler, errorHandler, {
-                    "badge": "true",
-                    "sound": "true",
-                    "alert": "true",
-                    "ecb": "onNotificationAPN"
-                }); // required!
-            }
-        } catch (err) {
-            txt = "There was an error on this page.\n\n";
-            txt += "Error description: " + err.message + "\n\n";
-            alert(txt);
+            pushNotification.register(successHandler, errorHandler, {
+                "senderID": "1003553143793",
+                "ecb": "onNotification"
+            }); // required!
+        } else {
+            pushNotification.register(tokenHandler, errorHandler, {
+                "badge": "true",
+                "sound": "true",
+                "alert": "true",
+                "ecb": "onNotificationAPN"
+            }); // required!
         }
+    } catch (err) {
+        txt = "There was an error on this page.\n\n";
+        txt += "Error description: " + err.message + "\n\n";
+        alert(txt);
     }
-    // handle APNS notifications for iOS
- 
+}
+// handle APNS notifications for iOS
+
 function onNotificationAPN(e) {
-        if (e.alert) {
-            // showing an alert also requires the org.apache.cordova.dialogs plugin
-            navigator.notification.alert(e.alert);
-        }
-        if (e.sound) {
-            // playing a sound also requires the org.apache.cordova.media plugin
-            var snd = new Media(e.sound);
-            snd.play();
-        }
-        if (e.badge) {
-            pushNotification.setApplicationIconBadgeNumber(successHandler, e.badge);
-        }
+    if (e.alert) {
+        // showing an alert also requires the org.apache.cordova.dialogs plugin
+        navigator.notification.alert(e.alert);
     }
-    // handle GCM notifications for Android
- 
+    if (e.sound) {
+        // playing a sound also requires the org.apache.cordova.media plugin
+        var snd = new Media(e.sound);
+        snd.play();
+    }
+    if (e.badge) {
+        pushNotification.setApplicationIconBadgeNumber(successHandler, e.badge);
+    }
+}
+// handle GCM notifications for Android
+
 function onNotification(e) {
     switch (e.event) {
         case 'registered':
@@ -71,15 +72,17 @@ function onNotification(e) {
             break;
     }
 }
- 
+
 function tokenHandler(result) {
     alert('device token = ' + result);
     // Your iOS push server needs to know the token before it can push to this device
     // here is where you might want to send it the token for later use.
 }
- 
-function successHandler(result) {}
- 
-function errorHandler(error) {}
+
+function successHandler(result) {
+}
+
+function errorHandler(error) {
+}
 document.addEventListener('deviceready', onDeviceReady, true);
 
