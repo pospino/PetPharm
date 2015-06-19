c96778dd2ddef81e1281588376d4b1e477be4646
prixmasol <pospino@prixmasol.com>
2015-06-19 00:03:59 -0500

    console log

--- js/PushCustom.js
+++ js/PushCustom.js
@@ -51,6 +51,7 @@ function onNotification(e) {
                 // Your GCM push server needs to know the regID before it can push to this device
                 // here is where you might want to send it the regID for later use.
                 alert("regID = " + e.regid);
+                console.log(e.regid);
             }
             break;
         case 'message':
